ad78d707f86f9bfaadfc5adf9e94e49b1b0aa65f
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-06-04 09:27:18 +0100

    update for role arn usage

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
-terraform {
-  required_version = ">= 0.11.0"
-}
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
+  assume_role { 
+
+    role_arn = var.role_arn 
+
+  } 
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   #availability_zone = "${var.aws_region}a"
 
   tags {
-    Name = "${var.name}"
+    Name = var.name
   }
 }
